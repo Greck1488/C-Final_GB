2d4cda4896f74b62eede8c66f18a64bac1d59c16
Sir Sergey <Sergeyandreev22@gmail.com>
2022-09-17 01:28:36 +0300

    1st part DONE

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] arrString = { };
+int newArrayLength = 0;
+
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Изначально заданный массив: ");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine($"{String.Join("; ",arrString )}");
+
+for (int i = 0; i < arrString.Length; i++)
+{
+    if (arrString[i].Length <= 3)
+    {
+        newArrayLength++;
+    }
+}
+
